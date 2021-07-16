46578c33f4b3bf5a0f7a50f1ec90effdc20d6055
Stephan Martin <designer2k2@gmail.com>
2021-07-16 12:49:10 +0200

    example with version check
    Version check and push to 1.0.4

--- src/EMUcanT4.h
+++ src/EMUcanT4.h
@@ -31,7 +31,7 @@
 
 #include <FlexCAN_T4.h>
 
-#define EMUCANT4_LIB_VERSION               (F("1.0.3"))
+#define EMUCANT4_LIB_VERSION               (F("1.0.4"))
 
 // Available data
 struct emu_data_t {
